1980d30c57970e56a1cf8f6fcb86ffd0464aaad8
Azamatov_Al <al.azam@live.com>
2021-05-10 23:29:15 +0300

    Initial commit

---
+++ src/SaveStudentOrder.java
@@ -0,0 +1,10 @@
+public class SaveStudentOrder {
+    public static void main(String[] args) {
+        saveStudentOrder();
+    }
+
+    static void saveStudentOrder() {
+        long answer = 199;
+        System.out.println(answer);
+    }
+}
